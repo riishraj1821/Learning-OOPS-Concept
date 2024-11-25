e4ed3c9c0fa03d91efc2c967ee1aaf0fe296b0be
riishraj1821 <rishirajchhalotre@gmail.com>
2024-11-25 12:36:24 +0530

    learning Class and Obejct

---
+++ src/Demo.java
@@ -0,0 +1,11 @@
+public class Demo {
+    public static void main(String[] args) {
+        Student st1 =  new Student();
+        st1.name = "raj";
+        st1.rollNo = 21;
+        st1.city = "Indore";
+
+        st1.study();
+        st1.Discription();
+    }
+}
---
+++ src/Student.java
@@ -0,0 +1,17 @@
+
+class Student {
+    String name;
+    int rollNo;
+    String city;
+
+
+    public void study() {
+        System.out.println("name of the student is" +" "+ name);
+    }
+
+    public void Discription(){
+        System.out.println("rollNo of the student is" +" " + rollNo);
+        System.out.println("rollNo of the student is" +" " + rollNo);
+    }
+
+}
